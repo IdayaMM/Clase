ed4501b94716a7856d076796ceb56bbc89614d67
Idaya mouta <idayamm17@educastur.es>
2023-11-23 19:28:43 +0100

    Hemos modificado el fichero de calcuradoraEurosgit status

--- CalculadoraEuros.java
+++ CalculadoraEuros.java
@@ -6,7 +6,7 @@ public class CalculadoraEuros {
 		final double FACTOR_CONV = 166.386; //Sirve para dividir. Las constantes se declaran en mayuscula
 		
 		System.out.println("Introduce una cantidad en pesetas: ");
-		int pesetas = teclado.nextInt();//Para que se lean las pesetas 
+		int pesetas = teclado.nextInt();//Para que se lean las pesetass
 		
 		System.out.printf("La cantidad en euros es %.2f",(pesetas/FACTOR_CONV));//%.2f para q salgan 2nº dps de la coma (lo q sta entre parentesis es la division de pesets a eur)
 		
